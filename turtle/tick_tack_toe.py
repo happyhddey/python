7c7e4330e3b31e659e172eb7419fb4d04b40742c
Hyunsoo1021 <103804458+Hyunsoo1021@users.noreply.github.com>
2022-07-02 17:59:46 +0900

    Update tick_tack_toe.py

--- turtle/tick_tack_toe.py
+++ turtle/tick_tack_toe.py
@@ -82,64 +82,116 @@ def make():
     fd(600)
 
 
+def draw_x():
+    pendown()
+    seth(45)
+    fd(90)
+    right(180)
+    fd(180)
+    right(180)
+    fd(90)
+    left(90)
+    fd(90)
+    left(180)
+    fd(180)
+
+
+numbs = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+def nanido_easy_ai():
+    global numbs
+    random_ai = random.randint(1, 9)
+    while random_ai not in numbs:
+        random_ai = random.randint(1, 9)
+    
+    
+
+
 def nanido_easy():
     make()
-    numbs = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
     def show(x, y):
+        global numbs
         if x >= -300 and x <= -100 and y >= 100 and y <= 300:
             if 1 in numbs:
                 print(1)
                 numbs.remove(1)
-                
+                penup()
+                goto(-200, 200)
+                draw_x()
             else:
                 pass
         elif x >= -100 and x <= 100 and y >= 100 and y <= 300:
             if 2 in numbs:
                 print(2)
                 numbs.remove(2)
+                penup()
+                goto(0, 200)
+                draw_x()
             else:
                 pass
         elif x >= 100 and x <= 300 and y >= 100 and y <= 300:
             if 3 in numbs:
                 print(3)
                 numbs.remove(3)
+                penup()
+                goto(200, 200)
+                draw_x()
             else:
                 pass
         elif x >= -300 and x <= -100 and y >= -100 and y <= 100:
             if 4 in numbs:
                 print(4)
                 numbs.remove(4)
+                penup()
+                goto(-200, 0)
+                draw_x()
             else:
                 pass
         elif x >= -100 and x <= 100 and y >= -100 and y <= 100:
             if 5 in numbs:
                 print(5)
                 numbs.remove(5)
+                penup()
+                goto(0, 0)
+                draw_x()
             else:
                 pass
         elif x >= 100 and x <= 300 and y >= -100 and y <= 100:
             if 6 in numbs:
                 print(6)
                 numbs.remove(6)
+                penup()
+                goto(200, 0)
+                draw_x()
             else:
                 pass
         elif x >= -300 and x <= -100 and y >= -300 and y <= -100:
             if 7 in numbs:
                 print(7)
                 numbs.remove(7)
+                penup()
+                goto(-200, -200)
+                draw_x()
             else:
                 pass
         elif x >= -100 and x <= 100 and y >= -300 and y <= -100:
             if 8 in numbs:
                 print(8)
                 numbs.remove(8)
+                penup()
+                goto(0, -200)
+                draw_x()
             else:
                 pass
         elif x >= 100 and x <= 300 and y >= -300 and y <= -100:
             if 9 in numbs:
                 print(9)
                 numbs.remove(9)
+                penup()
+                goto(200, -200)
+                draw_x()
             else:
                 pass
         else:
